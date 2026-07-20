5026c1f33d025e3dbd099f3cc4f35e9592e129b0
Vinicius <vinicius--cruz@hotmail.com>
2026-07-20 01:44:29 +0000

    Pages

--- copy/web/src/services/location.js
+++
@@ -1,7 +0,0 @@
-export function formatLocation(name, state, country, locale)
-{
-  const place = `${name}${state ? `, ${state}` : ''}`
-  const region = country ? new Intl.DisplayNames([locale], { type: 'region' }).of(country) : ''
-
-  return region ? `${place} - ${region}` : place
-}
